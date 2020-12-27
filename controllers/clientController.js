482cfaf9e16e006c7614851afac5e0534a96ff35
pmiluu <pmiluszkiewicz@gmail.com>
2020-12-27 19:17:18 +0100

    dodanie szablonu listy clientow

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -1,5 +1,13 @@
+const ClientRepository = require('../repository/sequelize/ClientRepository');
+
 exports.showClientList = (req, res, next) => {
-    res.render('pages/client/client-list', { navLocation: 'client' });
+    ClientRepository.getClients()
+        .then(clients => {
+            res.render('pages/client/client-list', {
+                clients: clients,
+                navLocation: 'client'
+            });
+        });
 }
 
 exports.showAddClientForm = (req, res, next) => {
