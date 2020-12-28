2a425697084b0d3f016c2243f311e74ac8bfff51
pmiluu <pmiluszkiewicz@gmail.com>
2020-12-28 21:34:33 +0100

    dodanie szablonu dodaj nowe wypozyczenie ( nie podlaczone )

--- controllers/rentalController.js
+++ controllers/rentalController.js
@@ -1,9 +1,41 @@
+const RentalRepository = require('../repository/sequelize/RentalRepository');
+const BoatRepository = require('../repository/sequelize/BoatRepository');
+const ClientRepository = require('../repository/sequelize/ClientRepository');
+
 exports.showRentalList = (req, res, next) => {
-    res.render('pages/rental/rental-list', { navLocation: 'rental' });
+    RentalRepository.getRentals()
+        .then(rentals => {
+            res.render('pages/rental/rental-list', {
+                rentals: rentals,
+                navLocation: 'rental'
+            });
+        });
 }
 exports.showAddRentalForm = (req, res, next) => {
-    res.render('pages/rental/rental-form', { navLocation: 'rental' });
+    let allClients, allBoats;
+    ClientRepository.getClients()
+        .then(cnts => {
+            allClients = cnts;
+            return BoatRepository.getBoats();
+        })
+        .then(bts => {
+            allBoats = bts;
+            res.render('pages/rental/rental-form', {
+                rental: {},
+                formMode: 'createNew',
+                allClients: allClients,
+                allBoats: allBoats,
+                pageTitle: 'New rentals',
+                btnLabel: 'Add rental',
+                formAction: '/rentals/add',
+                navLocation: 'rental'
+            });
+        });
 }
+
+
+
+
 exports.showRentalDetails = (req, res, next) => {
     res.render('pages/rental/rental-details', { navLocation: 'rental' });
 }
--- routes/rentalRoute.js
+++ routes/rentalRoute.js
@@ -5,6 +5,6 @@ const rentalControler = require('../controllers/rentalController');
 
 router.get('/', rentalControler.showRentalList);
 router.get('/add', rentalControler.showAddRentalForm);
-router.get('/details/:empId', rentalControler.showRentalDetails);
+router.get('/details/:rentId', rentalControler.showRentalDetails);
 
 module.exports = router;
